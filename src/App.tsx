e7e3978b70e38759ef40b13423c7ecb30ca34cd3
Ismail Rebah <ismail.rebah@gmail.com>
2024-10-07 22:01:18 +1300

    refactor: removing restriction in windows size

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,17 @@ import BedBunk from "./components/BedBunk";
 const App = () => {
   return (
     <main className="flex justify-center items-center w-screen h-screen">
-      <section
-        id="canvas-container"
-        className="w-[1200px] h-[1200px] border-2 border-black"
-      >
-        <Canvas frameloop="demand" camera={{ position: [-1, 1, 2.5], fov: 50 }}>
-          <color attach="background" args={["#f0f0f0"]} />
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[5, 10, 7]} intensity={1} castShadow />
-          <OrbitControls />
-          <BedBunkProvider>
-            <Center>
-              <BedBunk />
-            </Center>
-          </BedBunkProvider>
-        </Canvas>
-      </section>
+      <Canvas frameloop="demand" camera={{ position: [-1, 1, 2.5], fov: 50 }}>
+        <color attach="background" args={["#f0f0f0"]} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 10, 7]} intensity={1} castShadow />
+        <OrbitControls />
+        <BedBunkProvider>
+          <Center>
+            <BedBunk />
+          </Center>
+        </BedBunkProvider>
+      </Canvas>
     </main>
   );
 };
