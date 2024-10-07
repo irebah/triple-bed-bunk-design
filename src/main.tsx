41eddb87f7096412320377c6878513531867e619
Ismail Rebah <ismail.rebah@gmail.com>
2024-10-08 00:35:11 +1300

    feat: adding ga

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+
+const TRACKING_ID = "G-DVQGP1631E";
+
+ReactGA.initialize(TRACKING_ID);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
