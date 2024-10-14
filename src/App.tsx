0d8b16ffd4213f977804c04b9430486a94ad0e13
Ismail Rebah <ismail.rebah@gmail.com>
2024-10-14 16:52:35 +1300

    chore: centering bed in frame

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import BedBunk from "./components/BedBunk";
 const App = () => {
   return (
     <main className="flex justify-center items-center w-screen h-screen">
-      <Canvas frameloop="demand" camera={{ position: [-1, 1, 2.5], fov: 50 }}>
+      <Canvas frameloop="demand" camera={{ position: [-1.5, 3, 3.5], fov: 30 }}>
         <color attach="background" args={["#f0f0f0"]} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 10, 7]} intensity={1} castShadow />
